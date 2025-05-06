a1d01401c81b07b56afcdeaace31666c4278c105
dani-apc <daniel.castro@shawandpartners.com>
2025-05-06 15:42:19 -0300

    Enviando arquivo binario certinho

--- tdp-1/src/server.py
+++ tdp-1/src/server.py
@@ -1,14 +1,23 @@
 import socket
+import os
 
 localIP     = "127.0.0.1"
-
 localPort   = 20001
-
 bufferSize  = 1024
 
-msgFromServer       = "Mensagem recebida com sucesso"
+PAYLOAD_SYZE = 1232
 
+def bin2text(s): return "".join([chr(int(s[i:i+8],2)) for i in range(0,len(s),8)])
+
+path = os.getcwd() + '/tdp-1/src/'
+
+comandos_validos = ['GET']
+arquivos_validos = ['txt', 'dat']
+
+msgFromServer       = "Mensagem recebida com sucesso"
 bytesToSend         = str.encode(msgFromServer)
+
+errorMsg = "Erro no recebimento da mensagem"
 
 # Create a datagram socket
 
@@ -37,17 +46,51 @@ while(True):
 
     comando = clientMsg[:clientMsg.index('@')-1]
     print('Comando:', comando)
-    servidor = clientMsg[clientMsg.index('@')+1:clientMsg.index(':')]
-    print('Servidor:', servidor)
-    porta = clientMsg[clientMsg.index(':')+1:clientMsg.index('/')]
-    print('Porta:', porta)
+    # servidor = clientMsg[clientMsg.index('@')+1:clientMsg.index(':')]
+    # print('Servidor:', servidor)
+    # porta = clientMsg[clientMsg.index(':')+1:clientMsg.index('/')]
+    # print('Porta:', porta)
     tem_flag = clientMsg.find(' -d s[') != -1
     print('Flag:', tem_flag)
     arquivo = clientMsg[clientMsg.index('/')+1:] if not(tem_flag) else clientMsg[clientMsg.index('/')+1:clientMsg.index(' -d s[')]
     print('Arquivo:', arquivo)
+    extensao_arquivo = arquivo[arquivo.index('.')+1:]
+    print(extensao_arquivo)
 
     print(clientIP)
 
+    if extensao_arquivo not in arquivos_validos or comando not in comandos_validos:
+        print('Falha na requisição')
+        UDPServerSocket.sendto(str.encode(errorMsg), address)
+        continue
+
+    file_path = path+"/data/test_file.txt"
+    
+    if not(os.path.isfile(file_path)):
+        print('Falha na requisição')
+        UDPServerSocket.sendto(str.encode(errorMsg), address)
+        continue
+
+    f =  open(path+"/data/test_file.txt")
+    text = f.read()[0:10000]
+    bin_text = " ".join(f"{ord(i):08b}" for i in text).split(" ")
+
+    cursor = 0
+    text_lenght = len(bin_text)
+
+    while cursor < text_lenght:
+        payload = ""
+        for i in range(PAYLOAD_SYZE):
+            if cursor >= text_lenght:
+                break
+
+            cur_byte = bin_text[cursor][-8:]
+            payload += cur_byte
+        
+            cursor += 1
+
+        bytesToSend =  bin2text( payload).encode('utf-8')
+        UDPServerSocket.sendto(bytesToSend, address)
     # Sending a reply to client
 
-    UDPServerSocket.sendto(bytesToSend, address)
+    # UDPServerSocket.sendto(bytesToSend, address)
--- tdp-1/src/test.py
+++ tdp-1/src/test.py
@@ -33,3 +33,32 @@ if (tem_flag):
             indexes.append(int(s))
 
     print(indexes)
+
+
+import os
+path = os.getcwd() + '/tdp-1/src/'
+
+f = open(path+"/data/test_file.txt")
+text = f.read()[0:100]
+
+def text2bin(s): return '{:b}'.format(int(u'{s}'.encode('utf-8').encode('hex'), 16))
+# bin_text = " ".join(f"{ord(i):08b}" for i in text).split(" ")
+bin_text = text2bin(text)
+
+joined_text = ''.join(bin_text)
+print(''.join(bin_text))
+
+# def bin2text(s): return "".join([chr(int(x,2)) for x in [s[i:i+8] for i in range(0,len(s), 8)]])
+def bin2text(s): return "".join([chr(int(s[i:i+8],2)) for i in range(0,len(s),8)])
+
+
+print(bin2text(joined_text))
+# bin_text = text.encode("utf-8")
+
+# text = '01101001'
+# print(bin(int(text, base=2)))
+
+# bin_array = ' '.join(map(bin,bytearray(bin_text)))
+
+# # print(len(text))
+# print(bin_array)
